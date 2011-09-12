0aeef6e72d02e7f0b4308ca17bc2619f1ab28764
Oystein Djuvsland <oystein.djuvsland@gmail.com>
2011-09-12 17:54:55 +0200

    - adding license information - fixing warnings

--- gui/widgets/branch.h
+++ gui/widgets/branch.h
@@ -1,3 +1,23 @@
+/*
+    Library for controlling and configuring the electronics for the PHOS
+    detector at the ALICE Experiment
+    Copyright (C) 2011  Oystein Djuvsland <oystein.djuvsland@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #ifndef BRANCH_H
 #define BRANCH_H
 
@@ -21,11 +41,13 @@ public:
     virtual ~branch();
 
     void setGeometry(int x, int y) { QWidget::setGeometry(x, y, _kWidth, _kHeight);}
-
 private:
 
   /** Setup the internal widgets */
   void setupWidgets();
+  
+  /** Setup the slot connections */
+  void setupConnections();
   
   /** Branch id */
   Branch_t _branch;
@@ -42,6 +64,8 @@ private:
   const int _kWidth;
   const int _kHeight;
 
+  branch(const branch& );
+  branch operator=(const branch&);
   
 };
 
