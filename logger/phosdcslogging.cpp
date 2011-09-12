278680587e55529e8ccf24a5e255b30735d5a9e3
Oystein Djuvsland <oystein.djuvsland@gmail.com>
2011-09-12 17:55:22 +0200

    - adding license information - fixing warnings

--- logger/phosdcslogging.cpp
+++ logger/phosdcslogging.cpp
@@ -1,5 +1,27 @@
+/*
+    Library for controlling and configuring the electronics for the PHOS
+    detector at the ALICE Experiment
+    Copyright (C) 2011  Oystein Djuvsland <oystein.djuvsland@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #include "phosdcslogging.h"
+#pragma GCC diagnostic ignored "-Weffc++"
 #include "qmutex.h"
+#pragma GCC diagnostic pop
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -8,19 +30,21 @@
 #include <cstdlib>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <errno.h>
 #include "phosutils.h"
 
 using namespace std;
 
 phosDcsLogging* phosDcsLogging::fInstance = 0;
 
-phosDcsLogging::phosDcsLogging() : QObject(),
-        fFileLogLevel ( LOG_LEVEL_EXTREME_VERBOSE ),
-        fTerminalLogLevel ( LOG_LEVEL_VERBOSE ),
-        fLogViewerLevel ( LOG_LEVEL_DEFAULT ),
-        fLogFile(),
-        fLogViewerStringStream(),
-        fMutexLock ( 0 )
+phosDcsLogging::phosDcsLogging() : QObject()
+        ,fFileLogLevel ( LOG_LEVEL_EXTREME_VERBOSE )
+        ,fTerminalLogLevel ( LOG_LEVEL_VERBOSE )
+        ,fLogViewerLevel ( LOG_LEVEL_DEFAULT )
+        ,fLogFile()
+        ,fLogViewerStringStream()
+        ,fLogLevels(0)
+        ,fMutexLock ( 0 )
 {
 
     fMutexLock = new QMutex;
@@ -37,9 +61,14 @@ phosDcsLogging::phosDcsLogging() : QObject(),
     stringstream timestamp;
     stringstream datestamp;
 
-
-//TODO: Make safer solution to this!
-    mkdir ( "log/", 0777 );
+    if (mkdir ( "log/", 0777 ) < 0)
+    {
+        if (errno != EEXIST)
+        {
+            perror("Creating directory \"log/\"");
+            exit(-1);
+        }
+    }
 
     GetTimeStamp ( timestamp, datestamp );
     filename << "log/" << datestamp.str() << "_" << timestamp.str() << ".log";
@@ -58,7 +87,6 @@ phosDcsLogging::phosDcsLogging() : QObject(),
     Logging ( string ( "Starting Logger..." ), LOG_LEVEL_INFO );
 
 }
-
 
 phosDcsLogging::~phosDcsLogging()
 {
@@ -94,6 +122,7 @@ void phosDcsLogging::Logging ( string logmsg, unsigned int level, std::string fi
     {
         LogViewer ( tmplogmsg );
     }
+    emit(LoggingReceived());
     fMutexLock->unlock();
 }
 
@@ -144,7 +173,7 @@ void phosDcsLogging::GetTimeStamp ( stringstream& timestamp, stringstream& dates
         timestamp << timeinfo->tm_sec;
     }
     if ( datestamp )
-    {-
+    {
         datestamp.fill ( '0' );
         datestamp << timeinfo->tm_year + 1900 << ".";
         datestamp.width ( 2 );
@@ -161,7 +190,6 @@ void phosDcsLogging::AddFileAndLine ( string filename, int line, string& logmsg 
         int found= filename.find_last_of ( "/" );
         logmsg += " \t\t\t=> " + filename.substr ( found + 1 ) + ":" + to_string ( line ) ;
     }
-
 }
 
 #include "phosdcslogging.moc"
