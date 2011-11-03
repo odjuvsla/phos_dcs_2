12f7ddee789563c5a007a4db8c796dedc4651982
Oystein Djuvsland <oystein.djuvsland@gmail.com>
2011-11-03 16:46:52 +0100

    - fixes required due to change in filename

--- backend/phosdcsinterface.cpp
+++ backend/phosdcsinterface.cpp
@@ -18,7 +18,7 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "phosDcsInterface.h"
+#include "phosdcsinterface.h"
 
 phosDcsInterface::phosDcsInterface()
 {
