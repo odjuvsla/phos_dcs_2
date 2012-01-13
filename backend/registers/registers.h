3aefc470c483bcffbf79215cacc673f2052068b7
Henrik Qvigstad <henrik.qvigstad@cern.ch>
2012-01-13 14:15:01 +0100

    Changed includes in backend/registers/registers.h, see previous couple of commits.

--- backend/registers/registers.h
+++ backend/registers/registers.h
@@ -1,15 +1,15 @@
 #ifndef REGISTERS_H
 #define REGISTERS_H
 
-#include <actfeclist.h>
-#include <altrocfg1.h>
-#include <altrocfg2.h>
-#include <altroif.h>
-#include <bp_version.h>
-#include <rcu_version.h>
-#include <rcuid.h>
-#include <rdomod.h>
-#include <trgconf.h>
-#include <fecerra.h>
+#include <rcu/actfeclist.h>
+#include <rcu/altrocfg1.h>
+#include <rcu/altrocfg2.h>
+#include <rcu/altroif.h>
+#include <rcu/bp_version.h>
+#include <rcu/rcu_version.h>
+#include <rcu/rcuid.h>
+#include <rcu/rdomod.h>
+#include <rcu/trgconf.h>
+#include <rcu/fecerra.h>
 
 #endif
